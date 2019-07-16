a7f123a298b5516efb5394190fe584572ff798c6
Ricardo José Rodrigues da Silva <rjrsilva@fazenda.rj.gov.br>
2019-07-16 16:38:26 -0300

    Evoluindo o projeto

--- server.js
+++ server.js
@@ -1,32 +1,6 @@
-const express = require('express');
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function() {
     console.log('Servidor rodando na porta 3000');
 });
 
-app.get('/', function(req, resp) {
-    resp.send(`
-         <html>
-             <head>
-                 <meta charset="utf-8">
-             </head>
-             <body>
-                 <h1> Casa do Código </h1>
-             </body> 
-         </html>
-     `);
-});
-
-app.get('/livros', function(req, resp) {
-    resp.send(`
-         <html>
-             <head>
-                 <meta charset="utf-8">
-             </head>
-             <body>
-                 <h1> Listagem de livros </h1>
-             </body> 
-         </html>
-     `);
-});
